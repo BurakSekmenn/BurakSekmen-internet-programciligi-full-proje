d569bdaa00d2f87cafea7900fd92154e076384d5
Burak Sekmen <burakskmn07@gmail.com>
2024-03-09 12:37:32 +0300

    IGenericRepository,IService,IUnitOfWorks Eklemesi Yapıldı

---
+++ BurakSekmen.Core/Repository/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+
+namespace BurakSekmen.Core.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll();
+
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entity);
+
+        void Update(T entity);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entity);
+
+
+
+    }
+}
---
+++ BurakSekmen.Core/UnitOfWorks/IUnitOfWorks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurakSekmen.Core.UnitOfWorks
+{
+    public interface IUnitOfWorks
+    {
+        Task CommitAsync();
+        void Commit();
+    }
+}
