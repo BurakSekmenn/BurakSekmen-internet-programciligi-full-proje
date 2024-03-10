4d28ea0ff156ff9e725281edef3c08d6680884e1
Burak Sekmen <burakskmn07@gmail.com>
2024-03-10 20:23:38 +0300

    CustomResponse ValidateFilterAttribute ekledim.

---
+++ BurakSekmen.API/Filters/ValidateFilterAttribute.cs
@@ -0,0 +1,18 @@
+﻿using BurakSekmen.Core.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BurakSekmen.API.Filters
+{
+    public class ValidateFilterAttribute:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var error =  context.ModelState.Values.SelectMany(x => x.Errors).Select(x=>x.ErrorMessage).ToList();
+                context.Result = new BadRequestObjectResult(CustomeResponseDto<NoContentDto>.Fail(error,400));
+            }
+        }
+    }
+}
