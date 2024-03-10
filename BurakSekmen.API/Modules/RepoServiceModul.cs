8de6b0bcd3e16d31cdf7d07b5cc82a81fdbaf346
Burak Sekmen <burakskmn07@gmail.com>
2024-03-10 20:15:46 +0300

    Mapping işlemi ve AutoFac Kütüphanesi sayesinde bağlantıları tanımladık.

---
+++ BurakSekmen.API/Modules/RepoServiceModul.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using Autofac;
+using BurakSekmen.Core.Repository;
+using BurakSekmen.Core.Services;
+using BurakSekmen.Core.UnitOfWorks;
+using BurakSekmen.Repository.Context;
+using BurakSekmen.Repository.Repositories;
+using BurakSekmen.Repository.UnitOfWorks;
+using BurakSekmen.Service.Mapping;
+using BurakSekmen.Service.Services;
+using Module = Autofac.Module;
+
+namespace BurakSekmen.API.Modules
+{
+    public class RepoServiceModul:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>))
+                .InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(Service<>)).As(typeof(IService<>))
+                .InstancePerLifetimeScope();
+            builder.RegisterType<IUnitOfWorks>().As<IUnitOfWorks>();
+
+            var ApiAssembly = Assembly.GetExecutingAssembly();
+
+            var RepositoryAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+
+            var ServiceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+
+            builder.RegisterAssemblyTypes(ApiAssembly, RepositoryAssembly, ServiceAssembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(ApiAssembly, RepositoryAssembly, ServiceAssembly)
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+
+
+
+        }
+    }
+}
---
+++ BurakSekmen.Service/Mapping/MapProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BurakSekmen.Core.DTOs;
+using BurakSekmen.Core.Entity;
+
+namespace BurakSekmen.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<ProductFeature,ProductFeatureDto>().ReverseMap();
+            CreateMap<Person, PersonDto>().ReverseMap();
+            CreateMap<Sales,SalesDto>().ReverseMap();
+
+        }
+    }
+}
