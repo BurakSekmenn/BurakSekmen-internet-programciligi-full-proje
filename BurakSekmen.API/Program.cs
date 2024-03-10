5496ffa88c199e5095c25a4ee03cffc0e0f46e0d
Burak Sekmen <burakskmn07@gmail.com>
2024-03-10 20:33:39 +0300

    Program Cs Kısmına AutoFac eklemesi ve Validate kısmı eklendi.

--- BurakSekmen.API/Program.cs
+++ BurakSekmen.API/Program.cs
@@ -1,11 +1,32 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using BurakSekmen.API.Filters;
+using BurakSekmen.API.Modules;
 using BurakSekmen.Repository.Context;
+using BurakSekmen.Service.Mapping;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new ValidateFilterAttribute());
+}).AddFluentValidation(x =>
+{
+    x.RegisterValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+});
+
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});// ModelStateInvalidFilter'ý devre dýþý býrakýr.
+
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
@@ -15,9 +36,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     });
 });
 
-
-
-
+builder.Host.UseServiceProviderFactory(
+    new AutofacServiceProviderFactory()
+);
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new RepoServiceModul());
+});
 
 
 
@@ -25,6 +50,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped(typeof(NotFoundFilter<>));
+builder.Services.AddAutoMapper(typeof(MapProfile));
 
 var app = builder.Build();
 
